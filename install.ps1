8a5fbc6dbbca3f58df8cceac5be62d8b9c9db36e
alexander <oonray>
2025-10-03 11:46:15 +0000

    .

---
+++ install.ps1
@@ -0,0 +1,37 @@
+param {
+    [string]$startup='Automatic'
+    [string]$status='Running'
+    [string]$master
+    [string]$token
+    [bool]$ssh
+    [bool]$kube
+}
+
+$url={
+    $containerd="https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess/Install-Containerd.ps1",
+    $prepare="https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess/PrepareNode.ps1"
+}
+
+if($ssh){
+    Set-Service sshd -StartupType $startup -Status $status
+    Set-Service ssh-agent -StartupType $startup -Status $running
+
+    New-NetFirewallRule -Name sshd -DisplayName 'SSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+}
+
+if($kube){
+    iex (iwr -UseBasicParsing $url.containerd)
+    iex (iwr -UseBasicParsing $url.prepare)
+
+    if(![string]::IsNullOrEmpty($master)){
+        if(![string]::IsNullOrEmpty($token)){
+            kubeadm join --token $token $master:6443
+        }
+        else{
+            Write-Host "Must Specify Token"
+        }
+    }
+    else{
+        Write-Host "Must Specify Master addr"
+    }
+}
