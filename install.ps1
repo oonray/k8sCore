d0e3f3693ff8423e8fa2604d6c82a4d0aa71e559
alexander <oonray>
2025-10-03 13:37:08 +0000

    .

--- install.ps1
+++ install.ps1
@@ -8,6 +8,17 @@ param (
     [switch]$kube
 )
 
+$config="$env:ProgramData\ssh\sshd_config"
+$authorized_keys="$env:ProgramData\ssh\administrators_authorized_keys"
+
+$SetPowershell = @{
+    Path         = "HKLM:\SOFTWARE\OpenSSH"
+    Name         = "DefaultShell"
+    Value        = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
+    PropertyType = "String"
+    Force        = $true
+}
+
 $url=@{
     containerd="https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess/Install-Containerd.ps1"
     prepare="https://raw.githubusercontent.com/kubernetes-sigs/sig-windows-tools/master/hostprocess/PrepareNode.ps1"
@@ -17,7 +28,15 @@ if($ssh){
     Set-Service sshd -StartupType $startup -Status $status
     Set-Service ssh-agent -StartupType $startup -Status $status
 
-    powershell Add-Content -Force -Path $env:ProgramData\ssh\administrators_authorized_keys -Value '$authorizedKey';icacls.exe "$env:ProgramData\ssh\administrators_authorized_keys" /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
+    Add-Content -Force -Path $authorized_keys -Value "$key"
+    icacls.exe $authorized_keys /inheritance:r /grant "Administrators:F" /grant "SYSTEM:F"
+
+    Add-Content -Force -Path $config -Value "AllowAgentForwarding yes"
+    Add-Content -Force -Path $config -Value "PasswordAuthentication no"
+    Add-Content -Force -Path $config -Value "PermitEmptyPasswords no"
+    Add-Content -Force -Path $config -Value "PubkeyAuthentication yes"
+
+    New-ItemProperty @SetPowershell
 
    if (!(Get-NetFirewallRule -Name "sshd" -ErrorAction SilentlyContinue)) {
         New-NetFirewallRule -Name sshd -DisplayName 'SSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
