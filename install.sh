f33075426cc9fdf11eb53eeb398e1c5841a3b820
alexander <oonray>
2025-10-09 12:13:01 +0000

    .

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -o noglob
 
 if [ "$EUID" -ne 0 ]
-  then echo "Please run with sudo"
+  then echo "\nPlease run with sudo"
   exit
 fi
 
@@ -83,26 +83,26 @@ fatal()
 #-------------------------
 
 function fix_name(){
-    echo "$@" | sed -e 's/[][!#$%&()*;<=>?\_`{|}/[:space:]]//g;'
+    echo "\n$@" | sed -e 's/[][!#$%&()*;<=>?\_`{|}/[:space:]]//g;'
 }
 
 function mount(){
     if [ ! -d "$STORE_D" ]; then
-        echo "$STORE_D not found! making it"
+        echo "\n$STORE_D not found! making it"
         sudo mkdir -p $STORE_D
     fi
 
     if [ -z $MNTS_D ]; then
-        echo "$STORE_D has no disks mounted. mounting ..."
+        echo "\n$STORE_D has no disks mounted. mounting ..."
 
         if [ -z $MNTK_DEV ];then
-            echo "NO 50G Disk found to mount at $MNTK_D"
+            echo "\nNO 50G Disk found to mount at $MNTK_D"
             exit 1
         else
             local MNTK_DEV_P="/dev/${MNTK_DEV}1"
             local MNTK_UUID=$(sudo blkid $MNTK_DEV_P | awk '{print $2}')
             if [ -z $MNTK_UUID ]; then
-                echo "$MNTL_DEV_P Not found"
+                echo "\n$MNTL_DEV_P Not found"
                 exit 1
             else
                 if [ ! -z $(sudo grep -E "$MNTK_UUID") ]; then
@@ -113,13 +113,13 @@ EOF
             fi
         fi
         if [ -z $MNTL_DEV ];then
-            echo "NO 100G Disk found to mount at $MNTL_D"
+            echo "\nNO 100G Disk found to mount at $MNTL_D"
             exit 1
         else
             local MNTL_DEV_P="/dev/${MNTK_DEV}1"
             local MNTL_UUID=$(sudo blkid $MNTL_DEV_P | awk '{print $2}')
             if [ -z $MNTL_UUID ]; then
-                echo "$MNTL_DEV_P Not found"
+                echo "\n$MNTL_DEV_P Not found"
                 exit 1
             else
                 if [ ! -z $(sudo grep -E "$MNTL_UUID") ]; then
@@ -157,7 +157,7 @@ function help(){
 }
 
 function agent(){
-    echo "Installing agent"
+    echo "\nInstalling agent"
     dirs
     curl -sfL "https://get.k3s.io" | sh -s - \
         agent --token $1 --server https://$2:6443 --data-dir $DATA_D \
@@ -173,7 +173,7 @@ function server(){
 }
 
 function server_k3s(){
-    echo "USING k3s"
+    echo "\nUSING k3s"
     curl -sfL "https://get.k3s.io" | sh -s - \
         server --data-dir $DATA_D --secrets-encryption \
         --cluster-domain kube --default-local-storage-path $L_DATA_D \
@@ -186,7 +186,7 @@ function server_k3s(){
 
 
 function server_k8s(){
-    echo "USING k8s"
+    echo "\nUSING k8s"
     sudo apt-get install -y containerd sudo \
          apt-transport-https ca-certificates curl gpg 
 
@@ -197,7 +197,7 @@ net.ipv4.ip_forward=1
 net.netfilter.nf_conntrack_max=1048576
 EOF
     else
-        echo "net.ipv4.ip_forward IS ENABLED"
+        echo "\nnet.ipv4.ip_forward IS ENABLED"
     fi
 
     containerd config default \
@@ -241,7 +241,7 @@ EOF
     sudo systemctl daemon-reload
 
 
-    echo "Adding plugins"
+    echo "\nAdding plugins"
     ARCH=$(uname -m)
     case $ARCH in
         armv7*) ARCH="arm";;
@@ -253,7 +253,7 @@ EOF
     sudo curl -o /tmp/cni-plugin.tgz -L https://github.com/containernetworking/plugins/releases/download/v1.7.1/cni-plugins-linux-$ARCH-v1.7.1.tgz
     sudo tar -C /opt/cni/bin -xzf /tmp/cni-plugin.tgz
 
-    echo "Installing kubernetes"
+    echo "\nInstalling kubernetes"
     if [ ! -s "/etc/apt/keyrings/kubernetes-apt-keyring.gpg" ];then
         curl -fsSL \
              https://pkgs.k8s.io/core:/stable:/v1.34/deb/Release.key \
@@ -273,7 +273,7 @@ EOF
             kubernetes-cni wget curl vim git \
         && apt-mark hold kubelet kubeadm kubectl
 
-    echo "Adding modules"
+    echo "\nAdding modules"
     sudo dd status=none of=/etc/modules-load.d/k8s.conf <<EOF
 overlay
 br_netfilter
@@ -284,11 +284,11 @@ EOF
         && modprobe br_netfilter 
 
 
-    echo "Enabeling kubelet"
+    echo "\nEnabeling kubelet"
     sudo systemctl enable kubelet
     sudo kubeadm config images pull
 
-    echo "Cluster INIT"
+    echo "\nCluster INIT"
     kubeadm init --pod-network-cidr 10.244.0.0/16
         #--apiserver-advertise-address=$MASTER \
         #--node-ip $MASTER \
@@ -297,16 +297,16 @@ EOF
 }
 
 function server_k8s_uninstall(){
-    echo "Resetting kubeadm"
+    echo "\nResetting kubeadm"
     sudo kubeadm reset
 
-    echo "Uninstalling kubernetes and containerd"
+    echo "\nUninstalling kubernetes and containerd"
     sudo apt-get purge -y kubeadm kubectl kubelet \
             kubernetes-cni containerd \
          && apt-get autoremove -y \
          && apt-get clean -y
 
-    echo "Removing folders"
+    echo "\nRemoving folders"
     sudo rm -rf ~/.kube \
         /etc/cni \
         /etc/kubernetes \
@@ -318,7 +318,7 @@ function server_k8s_uninstall(){
         /var/lib/etcd2/ \
         /var/run/kubernetes
 
-    echo "Resetting iptables"
+    echo "\nResetting iptables"
     sudo iptables -F \
         && iptables -X \
         && iptables -t nat -F \
@@ -328,7 +328,7 @@ function server_k8s_uninstall(){
         && iptables -t mangle -F \
         && iptables -t mangle -X
 
-        echo "Reloading"
+        echo "\nReloading"
         sudo systemctl daemon-reload
 }
 
@@ -347,7 +347,7 @@ while getopts "$ARG_S" opt; do
     esac
 done
 
-echo "Kubernetes installer for Linux"
+echo "\nKubernetes installer for Linux"
 if $HELP
 then
     help
@@ -356,7 +356,7 @@ fi
 
 if (! $AGENT) && (! $SERVER) && (! $UNINSTALL) && (! $APPLY)
 then
-    echo "NO OPTIONS. Must be agent, server, apply or uninstall "
+    echo "\nNO OPTIONS. Must be agent, server, apply or uninstall "
     help
     exit 2
 
@@ -364,28 +364,28 @@ fi
 
 if $AGENT && $SERVER;
 then
-    echo "Cannot be both server and agent"
+    echo "\nCannot be both server and agent"
     help
     exit 2
 fi
 
 if $SERVER
 then
-    echo "Installing Server"
+    echo "\nInstalling Server"
     server
 fi
 
 if $AGENT
 then
-    echo "Installing Agent"
-    if [ -z $TOKEN ]; then echo "Needs token"; help; fi
-    if [ -z $MASTER ]; then echo "Needs master"; help; fi
+    echo "\nInstalling Agent"
+    if [ -z $TOKEN ]; then echo "\nNeeds token"; help; fi
+    if [ -z $MASTER ]; then echo "\nNeeds master"; help; fi
     agent $TOKEN $MASTER
 fi
 
 if $APPLY
 then
-    echo "Configuring Server and Applying core features"
+    echo "\nConfiguring Server and Applying core features"
     mkdir -p $HOME/.kube
     sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
@@ -399,9 +399,9 @@ fi
 
 if $UNINSTALL
 then
-    echo "Uninstalling Server"
+    echo "\nUninstalling Server"
     server_k8s_uninstall
     exit 0
 fi
 
-echo "No more Tasks!"
+echo "\nNo more Tasks!"
