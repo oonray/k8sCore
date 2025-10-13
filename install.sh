da3f2c210e8678bf29a2530853982f22aed378f2
alexander <oonray>
2025-10-13 10:08:09 +0000

    .

--- install.sh
+++ install.sh
@@ -434,8 +434,8 @@ then
     sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
     sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-    kubectl taint nodes --all node-role.kubernetes.io/control-plane-
-    kubectl label nodes --all node.kubernetes.io/exclude-from-external-load-balancers-
+    #kubectl taint nodes --all node-role.kubernetes.io/control-plane-
+    #kubectl label nodes --all node.kubernetes.io/exclude-from-external-load-balancers-
 
     kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 fi
